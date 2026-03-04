809385fd908515fa3983c4695ca7db17ca608ee8
Felix-John-code <aghedofelix13@gmail.com>
2026-03-04 19:54:07 +0100

    eks updated

--- modules/EKS/main.tf
+++ modules/EKS/main.tf
@@ -186,7 +186,7 @@ resource "aws_eks_access_entry" "felix_user" {
 resource "aws_eks_access_policy_association" "felix_admin" {
   cluster_name  = aws_eks_cluster.cluster.name
   principal_arn = "arn:aws:iam::430695042840:user/Rabonose"
-  policy_arn    = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
   access_scope {
     type = "cluster"
   }
