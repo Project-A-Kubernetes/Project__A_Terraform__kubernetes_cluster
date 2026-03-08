3204449549bae279db8a11b0b807ff990693837e
Felix-John-code <aghedofelix13@gmail.com>
2026-03-09 00:09:46 +0100

    DONE

--- environment/prod/network/main.tf
+++ environment/prod/network/main.tf
@@ -2,7 +2,7 @@
 locals {
   cluster_name = "felix-aws-cluster"
 }
-
+#this is my vp module, the foundation for everything that is running privately and publicly now
 module "VPC" {
   source           = "../../../modules/VPC"
   cidr             = "10.0.0.0/16"
